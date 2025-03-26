5994553ffef2c61ddf1931231c275681b3ec6b8e
Abhishek <Abhishekraut8697@gmail.com>
2025-03-27 02:45:44 +0530

    refactor server structure: move server setup to index.ts and remove old server.ts

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -43,6 +43,7 @@ export const registerController = async (req: Request, res: Response) => {
       message: "User registered successfully",
       userId: newUser._id,
     });
+    
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,9 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDb } from "./db/connectDb";
 
 dotenv.config();
-
-connectDb();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,10 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-import AuthRouter from './routes/auth.routes'
+import AuthRouter from "./routes/auth.routes";
 
-app.use('/api/v1/auth' , AuthRouter)
+app.use("/api/v1/auth", AuthRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+connectDb()
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 😎😎😎`);
+  });
+})
+.catch(()=>{
+  console.log(`something went wrong while connecting to databse`)
+})
