0cc7a1f242a8e69a2baef6b9c01678b70a2d1118
Abhishek <Abhishekraut8697@gmail.com>
2025-03-27 19:56:32 +0530

    feat: update InstagramLogin component with external links for Facebook login and password reset

--- client/src/pages/InstagramLogin.tsx
+++ client/src/pages/InstagramLogin.tsx
@@ -108,13 +108,13 @@ const InstagramLogin: React.FC = () => {
             >
               <path d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12c0-5.523-4.477-10-10-10z" />
             </svg>
-            Log in with Facebook
+            <a href="https://www.facebook.com/login">Log in with Facebook</a>
           </button>
         </div>
 
         <div className="text-center mt-4 ">
           <a href="#" className="text-sm text-white hover:underline">
-            Forgot password?
+           <a href="https://www.instagram.com/accounts/password/reset/">  Forgot password?</a>
           </a>
         </div>
       </div>
